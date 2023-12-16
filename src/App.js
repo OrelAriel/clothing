55e19b53e724b67222f49d9326bb953c21364d3a
Orel Ariel <orel@Orels-MacBook-Pro.local>
2023-12-16 12:17:50 +0200

    implement sign-in and shop component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+import Shop from './routes/shop/shop.component';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
